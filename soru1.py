8df6fabfc1fe3a9223401bca0e84d7aabd8fbbe9
atayigit <64311652+atayigit@users.noreply.github.com>
2020-11-30 20:44:43 +0300

    Update soru1.py

--- soru1.py
+++ soru1.py
@@ -1,13 +1,18 @@
 dizi=[]
 #Dizi uzunluğu belirleyelim
-dizi_uzunluğu=int(input("DİZİ UZUNLUĞUNU GİRİN"))
+dizi_uzunluğu=int(input("DİZİ UZUNLUĞUNU GİRİN:"))
 while len(dizi)!=(dizi_uzunluğu ):
     #diziye bir takım elemanlar ekleyelim
-    değer=int(input("listeye eklenecek bir sayı girin"))
+    değer=(input("listeye eklenecek bir değer girin:"))
     dizi.append(değer)
-    
+#soruda dizide bulunan bilgilerin sayı yada string olup olmadığı belirtilmemiş
+#python da liste içinde sayı ve karakter olabildiği için kolaylık sağlaması amacıyla string bıraktım
 #istenen dizi uzunluğuna ulaştık şimdi de listeden LİFO yöntemi ile veri çekelim
 veriçek=int(input("kaç eleman çekmek istediğinizi girin"))
+if veriçek>len(dizi):
+    print("dizide çekmek istediğiniz kadar eleman yok")
+    veriçek=int(input("kaç eleman çekmek istediğinizi girin"))
+
 sayaç=0
 while sayaç<=veriçek:
     print(dizi)
